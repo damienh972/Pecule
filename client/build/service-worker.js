958d7171a35608e5ac026fa8e090e009e354a9ca
damienh972 <helo.dams@gmail.com>
2021-02-24 14:38:09 +0100

    update: detect when a user have account(s) connected

--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.39a9761aa9a2c46d7ca396195e4dc1b3.js"
+  "/precache-manifest.bc782a92f43414d27649c7d7fbca74ff.js"
 );
 
 self.addEventListener('message', (event) => {
